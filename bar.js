770f558969bdbd8bfa9bd5bba8873f06c82ef3e5
Jawad6t5 <103382811+Jawad6t5@users.noreply.github.com>
2022-05-04 22:43:33 +0500

    initial commit

---
+++ bar.js
@@ -0,0 +1,17 @@
+function side(){
+    var x = document.getElementById("ssid");
+    if (x.className === "sidebar"){
+        x.className += "visible";
+    }
+    else {
+        x.className = "nav";
+    }
+
+
+
+
+
+
+
+
+}
